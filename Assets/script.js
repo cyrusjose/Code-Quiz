e35a059d10f4a6952a4c095f848172b28d406166
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-06 16:43:38 -0700

    Display questions onto page

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,12 @@
 var startButton = document.querySelector('#start-btn');
 var nextButton = document.querySelector('#next-btn');
 var questionContainerEl = document.querySelector('#question-container');
+var questionEl = document.querySelector('#question');
+var answerButtonEl = document.querySelector('#answer-buttons');
 var questions = [
     {
-       question: 'what is 2+2',
-       answers:  [
+       question: 'What is 2+2?',
+       answers: [
            {text: '4', correct: true},
            {text: '6', correct: false},
            {text: '22', correct: false},
@@ -12,22 +14,28 @@ var questions = [
        ]
     }
 ]
-var shuffled, currentQestion
+var shuffled, currentQestion;
 
 
 function start() {
     startButton.classList.add('hide');
     questionContainerEl.classList.remove('hide');
-    shuffled = question.sort(function () {
+    shuffled = questions.sort(function () {
         // The range will be from -0.5 to +0.5 and depending on the sign it will be sorted a certain way.
         Math.random() - 0.5;
     })
+    // This is the index number for the current question.
+    currentQestion = 0;
     next();
 
 }
 
 function next() {
+    displayQuestion(shuffled[currentQestion])
+}
 
+function displayQuestion(question) {
+ questionEl.innerText = question.question;
 }
 
 function selectAnswer() {
