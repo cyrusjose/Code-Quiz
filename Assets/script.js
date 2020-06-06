86ac3a42a97ad601e212de475cc8926e6efad5fb
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-06 16:27:12 -0700

    Added randomization

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,28 @@
 var startButton = document.querySelector('#start-btn');
 var nextButton = document.querySelector('#next-btn');
 var questionContainerEl = document.querySelector('#question-container');
+var questions = [
+    {
+       question: 'what is 2+2',
+       answers:  [
+           {text: '4', correct: true},
+           {text: '6', correct: false},
+           {text: '22', correct: false},
+           {text: '0', correct: false},
+       ]
+    }
+]
+var shuffled, currentQestion
 
 
 function start() {
     startButton.classList.add('hide');
     questionContainerEl.classList.remove('hide');
+    shuffled = question.sort(function () {
+        // The range will be from -0.5 to +0.5 and depending on the sign it will be sorted a certain way.
+        Math.random() - 0.5;
+    })
+    next();
 
 }
 
