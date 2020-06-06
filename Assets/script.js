102081e63878ae3bbebb5a3a7c16daa892186f8d
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-06 15:18:44 -0700

    Added event listeners to start and next button

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,7 @@
-var startButton = document.querySelector('#start-btn')
+var startButton = document.querySelector('#start-btn');
+var nextButton = document.querySelector('#next-btn');
+
+
 function start() {
 
 }
@@ -10,3 +13,6 @@ function next() {
 function selectAnswer() {
 
 }
+
+startButton.addEventListener('click', start);
+nextButton.addEventListener('click', next);
