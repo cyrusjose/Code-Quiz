9aa5577cd08af96d127f5a9c39138ceb4771a0a7
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-06 23:08:50 -0700

    Removed score penalty

--- Assets/script.js
+++ Assets/script.js
@@ -31,7 +31,7 @@ var shuffled, currentQestion;
 var score = 0;
 
 function timer() {
-    var sec = 2;
+    var sec = 100;
     var timer = setInterval(function() {
         if (sec >= 0) {
             timerEl.innerText = 'Timer: ' + sec + ' seconds';
@@ -101,10 +101,6 @@ function selectAnswer(event) {
     const correct = selected.dataset.correct;
     if (correct) {
         score++;
-    } else {
-        if (score > 0) {
-            score--;
-        };
     }
     statClass(document.body, correct);
     // Ensure that what we get back is an array so that we can use forEach to loop through the array.
