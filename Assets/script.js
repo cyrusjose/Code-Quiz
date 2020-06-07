123dd171aa15a04799b24b1c168d44838447f2b7
Cyrus Jose <cyrusjose@Cyruss-Mac-mini.local>
2020-06-06 17:26:38 -0700

    Made a restart button

--- Assets/script.js
+++ Assets/script.js
@@ -5,13 +5,13 @@ var questionEl = document.querySelector('#question');
 var answerButtonsEl = document.querySelector('#answer-buttons');
 var questions = [
     {
-       question: 'What is 2+2?',
-       answers: [
-           {text: '4', correct: true},
-           {text: '6', correct: false},
-           {text: '22', correct: false},
-           {text: '0', correct: false},
-       ]
+        question: 'What is 2+2?',
+        answers: [
+            { text: '4', correct: true },
+            { text: '6', correct: false },
+            { text: '22', correct: false },
+            { text: '0', correct: false },
+        ]
     }
 ]
 var shuffled, currentQestion;
@@ -32,40 +32,47 @@ function start() {
 
 function next() {
     displayQuestion(shuffled[currentQestion]);
+    currentQestion++;
+
 }
 
 function displayQuestion(question) {
- questionEl.innerText = question.question;
- question.answers.forEach(answer => {
-    // create buttons using const because it will only apply here. 
-    const button = document.createElement('button');
-    // Setting text in buttons to what we have in our questions object
-    button.innerText = answer.text;
-    // adding the class "btn" to the newly created buttons.
-    button.classList.add('btn');
-    // Check to see if answer is correct
-    if (answer.correct){
-        // Add a data attribute to our button with the value of correct
-        // This is to check if the selcted answer is correct.
-        button.dataset.correct = answer.correct;
-    }
-    button.addEventListener('click', selectAnswer);
-    answerButtonsEl.appendChild(button);
-});
+    questionEl.innerText = question.question;
+    question.answers.forEach(answer => {
+        // create buttons using const because it will only apply here. 
+        const button = document.createElement('button');
+        // Setting text in buttons to what we have in our questions object
+        button.innerText = answer.text;
+        // adding the class "btn" to the newly created buttons.
+        button.classList.add('btn');
+        // Check to see if answer is correct
+        if (answer.correct) {
+            // Add a data attribute to our button with the value of correct
+            // This is to check if the selcted answer is correct.
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsEl.appendChild(button);
+    });
 }
 
 function selectAnswer(event) {
     const selected = event.target;
     const correct = selected.dataset.correct;
     statClass(document.body, correct);
-    Array.from(answerButtonsEl.children).forEach(button =>{
+    Array.from(answerButtonsEl.children).forEach(button => {
         statClass(button, button.dataset.correct);
     });
-    nextButton.classList.remove('hide');
+    if (shuffled.length > currentQestion + 1) {
+        nextButton.classList.remove('hide');
+    } else {
+        startButton.innerText = 'restart';
+        startButton.classList.remove('hide');
+    }
 }
 
-function statClass(element, correct){
-    if(correct){
+function statClass(element, correct) {
+    if (correct) {
         element.classList.add('correct');
     } else {
         element.classList.add('incorrect');
