226e8dfb873c26615cf50f49b9d108625ce56419
Cyrus Jose <cyrus.jose@outlook.com>
2020-06-07 20:18:09 -0700

    Added more quesitons

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,9 @@
 var startButton = document.querySelector('#start-btn');
 var nextButton = document.querySelector('#next-btn');
 var getScoreButton = document.querySelector('#score-btn');
+var clearButton = document.querySelector('#clear');
 var showScore = document.querySelector('#score');
+var playerScore = document.querySelector('#player-score');
 var questionContainerEl = document.querySelector('#question-container');
 var controlPanel = document.querySelector('.controls')
 var questionEl = document.querySelector('#question');
@@ -10,30 +12,185 @@ var submitButtonEl = document.querySelector('#submit');
 var homeButtonEl = document.querySelector('#ret');
 var timerEl = document.querySelector('#timer');
 var timeText = document.querySelector('#time');
-var playerName = localStorage.getItem('User Name');
-var scoreValue = localStorage.getItem('Score');
 var score = 0;
 var shuffled, currentQestion;
 var questions = [{
-    question: 'What is 2+2?',
+    question: 'What does "hsl" stand for?',
     answers: [
-        { text: '4', correct: true },
-        { text: '6', correct: false },
-        { text: '22', correct: false },
-        { text: '0', correct: false },
+        { text: 'Hue, Saturation, Lightness', correct: true },
+        { text: 'Hue, Saturation, Limit', correct: false },
+        { text: 'Heavy, Saturation, Light', correct: false },
+        { text: 'Handed, Saturn, Lemon', correct: false },
     ]
 },
 {
-    question: 'What is 3+2?',
+    question: 'What does the "a" in "hsla" an acronym for?',
     answers: [
-        { text: '4', correct: false },
-        { text: '6', correct: false },
-        { text: '5', correct: true },
-        { text: '0', correct: false },
+        { text: 'Alpine', correct: false },
+        { text: 'Allow', correct: false },
+        { text: 'Alpha', correct: true },
+        { text: 'Aggregate', correct: false },
     ]
-}
+},
+{
+    question: 'What does the "a" in "hsla" mean?',
+    answers: [
+        { text: 'The Contrast', correct: false },
+        { text: 'The Opacity', correct: true },
+        { text: 'No Meaning', correct: false },
+        { text: 'A variable placeholder', correct: false },
+    ]
+},
+{
+    question: 'How do you declare a variable?',
+    answers: [
+        { text: 'var hello', correct: true },
+        { text: 'var hello = 4', correct: false },
+        { text: 'var', correct: false },
+        { text: 'var 1123.hello= 5', correct: false },
+    ]
+},
+{
+    question: 'How do you assign a variable?',
+    answers: [
+        { text: 'var hello', correct: false },
+        { text: 'hello = 4', correct: true },
+        { text: 'var', correct: false },
+        { text: 'var 1123.hello= 5', correct: false },
+    ]
+},
+{
+    question: 'Which of the following is the literal notation for creating objects?',
+    answers: [
+        { text: 'var hotel = {};', correct: true },
+        { text: 'var hotelName = hotel.name', correct: false },
+        { text: 'function Hotel() {}', correct: false },
+        { text: 'var hotel = []', correct: false },
+    ]
+},
+{
+    question: 'Which of the following is the constructor notation for creating objects?',
+    answers: [
+        { text: 'var hotel = {};', correct: false },
+        { text: 'var hotelName = hotel.name', correct: false },
+        { text: 'function Hotel() {};', correct: true },
+        { text: 'var hotel = []', correct: false },
+    ]
+},
+{
+    question: 'Which of the following is NOT a logical operator?',
+    answers: [
+        { text: '||', correct: false },
+        { text: '&&', correct: false },
+        { text: '=', correct: true },
+        { text: '!', correct: false },
+    ]
+},
+{
+    question: 'Which of the following is NOT a mouse event?',
+    answers: [
+        { text: 'click', correct: false },
+        { text: 'mouseover', correct: false },
+        { text: 'dblclick', correct: false },
+        { text: 'keypress', correct: true },
+    ]
+},
+{
+    question: 'Which of the following is commonly used to test your code?',
+    answers: [
+        { text: 'console.log();', correct: true },
+        { text: 'window.location.reload()', correct: false },
+        { text: 'forEach()', correct: false },
+        { text: 'cout', correct: false },
+    ]
+},
+{
+    question: 'Which of the following is used to add content AFTER the selected element?',
+    answers: [
+        { text: '.before();', correct: false },
+        { text: '.append();', correct: false },
+        { text: '.prepend();', correct: false },
+        { text: '.after();', correct: true },
+    ]
+},
+{
+    question: 'Which of the following is used to add content AFTER the closing tag?',
+    answers: [
+        { text: '.before();', correct: false },
+        { text: '.append();', correct: true },
+        { text: '.prepend();', correct: false },
+        { text: '.after();', correct: false },
+    ]
+},
+{
+    question: 'Which of the following is used to add content BEFORE the selected element?',
+    answers: [
+        { text: '.before();', correct: true },
+        { text: '.after();', correct: false },
+        { text: '.prepend();', correct: false },
+        { text: '.after();', correct: false },
+    ]
+},
+{
+    question: 'Which of the following is used to add content BEFORE the opening tag?',
+    answers: [
+        { text: '.before();', correct: false },
+        { text: '.after();', correct: false },
+        { text: '.prepend();', correct: true },
+        { text: '.after();', correct: false },
+    ]
+},
+
 ]
 var sec = 100;
+var scoresAndInitials = [];
+function initials() {
+    playerScore.innerHTML = '';
+    for (var i = 0; i < scoresAndInitials.length; i++) {
+        var initials = scoresAndInitials[i];
+        var li = document.createElement('li');
+        li.textContent = 'Player: ' + initials.toUpperCase() + '\n' + 'score: '+ score;
+        li.setAttribute('data-index', i);
+        playerScore.appendChild(li);
+    }
+}
+
+function init() {
+    var storedInitials = JSON.parse(localStorage.getItem('scoresAndInitials'));
+
+    if (storedInitials !== null) {
+        scoresAndInitials = storedInitials;
+    }
+    initials();
+}
+
+function storeInit() {
+    localStorage.setItem('scoresAndInitials', JSON.stringify(scoresAndInitials))
+}
+function showScoreList(e) {
+    e.preventDefault();
+
+    var userName = document.querySelector('#userName').value.trim();
+    if (userName === ''){
+        return;
+    }
+    scoresAndInitials.push(userName);
+    userName.value = '';
+
+    document.querySelector('.score').classList.add('hide');
+    document.querySelector('#submit').classList.add('hide');
+    document.querySelector('#ret').classList.remove('hide');
+    document.querySelector('#clear').classList.remove('hide');
+    hideItems();
+    reset();
+    timeText.remove();
+    getScoreButton.remove();
+    document.querySelector('#score-list').classList.remove('hide');
+    storeInit();
+    initials();
+}
+
+
 
 function setTimer(timer) {
     var timer = setInterval(function() {
@@ -59,7 +216,6 @@ function hideItems() {
 }
 
 function start() {
-    setTimer();
     startButton.classList.add('hide');
     questionContainerEl.classList.remove('hide');
     shuffled = questions.sort(function() {
@@ -69,7 +225,7 @@ function start() {
     // This is the index number for the current question.
     currentQestion = 0;
     next();
-
+    setTimer();
 }
 
 function next() {
@@ -116,7 +272,6 @@ function selectAnswer(event, timer) {
     });
     // Check to see if the question is the last question
     if (shuffled.length > currentQestion) {
-        // nextButton.classList.remove('hide');
         nextButton.classList.remove('hide')
     } else {
         getScoreButton.classList.remove('hide');
@@ -156,106 +311,15 @@ function finalScore() {
     showScore.innerText = 'Your score is: ' + score;
 }
 
-function showScoreList(e) {
-    e.preventDefault();
-    document.querySelector('.score').classList.add('hide');
-    document.querySelector('#submit').classList.add('hide');
-    document.querySelector('#ret').classList.remove('hide');
-    hideItems();
-    reset();
-    timeText.remove();
-    getScoreButton.remove();
-    document.querySelector('#score-list').classList.remove('hide');
-    var player = document.querySelector('#player-score');
-    var userName = document.querySelector('#userName').value;
-    player.innerText = 'Player: ' + userName.toUpperCase() + '\n' + 'Score: ' + score;
-    homeButtonEl.addEventListener('click', function() {
-        window.location.reload();
-    });
-}
-
-// var todos = [];
-
-// init();
-
-// function renderTodos() {
-//   // Clear todoList element and update todoCountSpan
-//   todoList.innerHTML = "";
-//   todoCountSpan.textContent = todos.length;
-
-//   // Render a new li for each todo
-//   for (var i = 0; i < todos.length; i++) {
-//     var todo = todos[i];
-
-//     var li = document.createElement("li");
-//     li.textContent = todo;
-//     li.setAttribute("data-index", i);
-
-//     var button = document.createElement("button");
-//     button.textContent = "Complete";
-
-//     li.appendChild(button);
-//     todoList.appendChild(li);
-//   }
-// }
-
-// function init() {
-//   // Get stored todos from localStorage
-//   // Parsing the JSON string to an object
-//   var storedTodos = JSON.parse(localStorage.getItem("todos"));
-
-//   // If todos were retrieved from localStorage, update the todos array to it
-//   if (storedTodos !== null) {
-//     todos = storedTodos;
-//   }
-
-//   // Render todos to the DOM
-//   renderTodos();
-// }
-
-// function storeTodos() {
-//   // Stringify and set "todos" key in localStorage to todos array
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
-
-// // When form is submitted...
-// todoForm.addEventListener("submit", function(event) {
-//   event.preventDefault();
-
-//   var todoText = todoInput.value.trim();
-
-//   // Return from function early if submitted todoText is blank
-//   if (todoText === "") {
-//     return;
-//   }
-
-//   // Add new todoText to todos array, clear the input
-//   todos.push(todoText);
-//   todoInput.value = "";
-
-//   // Store updated todos in localStorage, re-render the list
-//   storeTodos();
-//   renderTodos();
-// });
-
-// // When a element inside of the todoList is clicked...
-// todoList.addEventListener("click", function(event) {
-//   var element = event.target;
-
-//   // If that element is a button...
-//   if (element.matches("button") === true) {
-//     // Get its data-index value and remove the todo element from the list
-//     var index = element.parentElement.getAttribute("data-index");
-//     todos.splice(index, 1);
-
-//     // Store updated todos in localStorage, re-render the list
-//     storeTodos();
-//     renderTodos();
-//   }
-// });
-
 
 startButton.addEventListener('click', start);
 nextButton.addEventListener('click', next);
 getScoreButton.addEventListener('click', finalScore)
 submitButtonEl.addEventListener('click', showScoreList)
+homeButtonEl.addEventListener('click', function() {
+    window.location.reload();
+});
+clearButton.addEventListener('click', function() {
+    document.querySelector('#player-score').remove();
+})
+
